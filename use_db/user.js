4e3ceda57f2c65928ca360301705ac160418c010
Yoon HanSol <yoonhs3434@naver.com>
2019-05-11 18:07:44 +0900

    add deleting

--- use_db/user.js
+++ use_db/user.js
@@ -63,4 +63,20 @@ router.post('/insert', (req, res) => {
     });
 });
 
+router.delete('/delete/:id', (req, res) => {
+    const u_id = req.body.id || req.params.id;
+
+    let query = "DELETE FROM user WHERE id = ?;"
+    let param = [u_id];
+    conn.query(query, param, (err, result) => {
+        if(!err) {
+            console.log('delete success');
+            res_data = JSON.parse(JSON.stringify(result));
+            res.json(res_data);
+        } else {
+            console.log('Error while performing Query.', err);
+        }
+    });
+});
+
 module.exports = router;
