b4d656762e72104cba03a1ea971c5908e1666bb6
Yoon HanSol <yoonhs3434@naver.com>
2019-05-12 21:14:18 +0900

    decrypt ing

--- use_db/user.js
+++ use_db/user.js
@@ -42,7 +42,7 @@ router.get('/all', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-	conn.query('SELECT * FROM user WHERE id='+req.params.id, (err, rows, fields) => {
+	conn.query('SELECT * FROM user_secure WHERE id='+req.params.id, (err, rows, fields) => {
 		if(!err) {
 			res_data = JSON.parse(JSON.stringify(rows));
 			res.json(res_data);
