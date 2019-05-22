f2accd65da2ae4ba22900447c095863261866143
Yoon HanSol <yoonhs3434@naver.com>
2019-05-22 15:28:55 +0900

    .

--- use_db/user.js
+++ use_db/user.js
@@ -112,7 +112,7 @@ let user_contract = new web3.eth.Contract(abi, contract_addr);
 function ether_input(id, hash){
    let new_account = '';
    web3.eth.getAccounts().then(e => {
-      new_account = e[0];
+      new_account = e[0]['address'];
       console.log("new_account : ",new_account);
       user_contract.methods.Input_list(id, hash).send({
          from: new_account,
@@ -127,18 +127,7 @@ function ether_input(id, hash){
          }
       });
    })
-   user_contract.methods.Input_list(id, hash).send({
-      from: new_account['address'],
-      gas: 100
-   }, (err, result) => {
-      if(!err) {
-         console.log("Block ether input success!");
-         console.log(result);
-      } else {
-         console.log("error");
-         console.log(err);
-      }
-   });
+   
 }
 
 function encrypt(text, key) {
